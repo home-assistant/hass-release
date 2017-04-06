3bc1eb21aa2fe6983ff5f4d17fde2418ceef0531
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-04-06 08:47:32 -0700

    Make passing in a release possible

--- hassrelease/__main__.py
+++ hassrelease/__main__.py
@@ -1,12 +1,20 @@
+import sys
+
 from .github import get_session
 from .model import Release, PRCache
 from .changelog import generate
 
 
 def main():
+    if len(sys.argv) < 2:
+        sys.stderr.write(
+            'No release specified. Please run '
+            'python3 -m hassrelease <release>\n'.format(sys.argv[0]))
+        sys.exit(1)
+
     github = get_session()
     repo = github.repository('home-assistant', 'home-assistant')
-    release = Release('0.41')
+    release = Release(sys.argv[1])
     prs = PRCache(repo)
 
     generate(release, prs)
