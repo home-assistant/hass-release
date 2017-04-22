2619745149f7be54f2447edd058a09463a3b3c68
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-04-21 21:58:04 -0700

    Add links to docs

--- hassrelease/changelog.py
+++ hassrelease/changelog.py
@@ -3,11 +3,14 @@ from collections import OrderedDict
 from .users import update_users_with_release
 
 OUTPUT = 'data/release-{}.md'
-USER_TEMPLATE = '([@{0}])'
 INFO_TEMPLATE = '([@{0}] - [#{1}])'
 PR_TEMPLATE = '([#{0}])'
+DOC_TEMPLATE = '([{0} docs])'
 LINK_DEF_USER = '[@{0}]: https://github.com/{0}'
 LINK_DEF_PR = '[#{0}]: https://github.com/home-assistant/home-assistant/pull/{0}'
+LINK_DEF_DOC = '[{0} docs]: https://home-assistant.io/components/{0}'
+IGNORE_DOCS = set(['discovery'])
+DOCS_LABELS = ['platform: ', 'component: ']
 
 
 def generate(release, prs):
@@ -19,23 +22,32 @@ def generate(release, prs):
 
     changes = []
     links = set()
-
     for line in release.log_lines():
         labels = []
         parts = ['-', line.message]
         links.add(LINK_DEF_USER.format(users[line.email]))
 
+        # Filter out git commits that are not merge commits
         if line.pr is None:
-            parts.append(USER_TEMPLATE.format(users[line.email]))
-        else:
-            parts.append(INFO_TEMPLATE.format(users[line.email], line.pr))
-            links.add(LINK_DEF_PR.format(line.pr))
+            continue
 
-            labels = [label.name for label in prs.get(line.pr).labels()]
+        parts.append(INFO_TEMPLATE.format(users[line.email], line.pr))
+        links.add(LINK_DEF_PR.format(line.pr))
+
+        labels = [label.name for label in prs.get(line.pr).labels()]
 
         for label in labels:
             if label in label_groups:
                 parts.append("({})".format(label))
+            for doc_label in DOCS_LABELS:
+                if label.startswith(doc_label):
+                    doc = label[len(doc_label):]
+
+                    if doc in IGNORE_DOCS:
+                        continue
+
+                    parts.append(DOC_TEMPLATE.format(doc))
+                    links.add(LINK_DEF_DOC.format(doc))
 
         msg = ' '.join(parts)
         changes.append(msg)
