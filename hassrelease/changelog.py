90d2ab5887894885c4fefd1149179bcfa09f9cae
cdce8p <30130371+cdce8p@users.noreply.github.com>
2018-04-25 16:19:22 +0200

    Made generating beta changelogs easier
    * 'release_notes 0.68.0b1' now possible -> version comparison for milestone
    * beta release branches now generate own file 'release-0-68-0b1.md'
    * 'release_notes 0.68' will still generate 'release-0-68.md'

--- hassrelease/changelog.py
+++ hassrelease/changelog.py
@@ -97,7 +97,8 @@ def generate(release, prs):
         pr = prs.get(line.pr)
 
         if (pr.milestone is not None and
-                StrictVersion(pr.milestone.title) != release.version):
+            StrictVersion(pr.milestone.title).version !=
+                release.version.version):  # Ignore beta version tag
             continue
 
         labels = [label.name for label in pr.labels()]
