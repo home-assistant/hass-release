5cad3ce239f3caa38ba84aa1d759e33a56149a49
WofWca <39462442+WofWca@users.noreply.github.com>
2018-10-06 19:03:51 +0700

    Remove obsolete comment

--- hassrelease/credits.py
+++ hassrelease/credits.py
@@ -6,10 +6,6 @@ def generate_credits():
     process = subprocess.run(
         "node update_credits.js",
         shell=True,
-        # 'update_credits.js' has the following text in it:
-        # 'fs.writeFile('../source/developers/credits.markdown
-        # 'credits_generator' folder from home-assistant.io is going to be
-        # removed, so we cd to another folder with the same level.
         cwd="credits_generator"
     )
     if process.returncode != 0:
