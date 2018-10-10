4d6b1a83e7b0d3ea81c5b1c798df611a77edb777
WofWca <39462442+WofWca@users.noreply.github.com>
2018-10-10 20:48:29 +0700

    Rewrite users.py
    Make functions more universal and intuitive.

--- hassrelease/users.py
+++ hassrelease/users.py
@@ -2,23 +2,30 @@
 from .const import GH_NO_EMAIL_SUFFIX, USERS_FILE
 
 
-def load_users():
-    users = {}
-
-    try:
-        with open(USERS_FILE) as inp:
-            for lin in inp:
-                if ',' not in lin:
-                    lin = lin.strip() + ','
-                email, github = [val.strip() for val in lin.split(',')]
-                users[email] = github
-    except FileNotFoundError:
-        pass
-
-    return users
+def read_csv_to_dict(filename: str):
+    data = {}
+    with open(filename) as inp:
+        for lin in inp:
+            if ',' not in lin:
+                lin = lin.strip() + ','
+            key, value = [val.strip() for val in lin.split(',')]
+            data[key] = value
+    return data
 
 
-def resolve_user(users, email, *, pr=None, prs=None, ask_input=True, context=None):
+def write_login_by_email_file(login_by_email_dict: dict):
+    with open(USERS_FILE, 'wt') as outp:
+        for email, github in sorted(login_by_email_dict.items()):  # TODO does it need to be sorted? Just use list(login_by_email_dict.items())?
+            outp.write('{},{}\n'.format(email, github))
+
+
+def append_dict_to_csv(data: dict, filename: str):
+    with open(filename, 'a') as file:
+        for key, value in list(data.items()):
+            file.write('{},{}\n'.format(key, value))
+
+
+def resolve_login(users, email, *, pr=None, prs=None, ask_input=True, context=None):
     """Resolves boolean if user added."""
     if email in users:
         return False
@@ -49,29 +56,30 @@ def resolve_user(users, email, *, pr=None, prs=None, ask_input=True, context=Non
 
 
 def update_users_with_release(release, prs):
-    users = load_users()
+    try:
+        users = read_csv_to_dict(USERS_FILE)
+    except FileNotFoundError:
+        pass
 
     added = 0
     ask_input = True
 
     for line in release.log_lines():
         try:
-            if resolve_user(users, line.email, pr=line.pr,
-                            prs=prs, ask_input=ask_input, context=line.line):
+            if resolve_login(users, line.email, pr=line.pr,
+                             prs=prs, ask_input=ask_input, context=line.line):
                 added += 1
         except KeyboardInterrupt:
             ask_input = False
 
     for email, github in sorted(users.items()):
         if not github:
-            if resolve_user(users, email, ask_input=ask_input):
+            if resolve_login(users, email, ask_input=ask_input):
                 added += 1
 
     if added > 0:
         print("Added {} users".format(added))
-        with open(USERS_FILE, 'wt') as outp:
-            for email, github in sorted(users.items()):
-                outp.write('{},{}\n'.format(email, github))
+        write_login_by_email_file(users)  # TODO replace by append_to_csv()?
     else:
         print("Users up to date")
 
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,7 @@
-from hassrelease.users import resolve_user
+from hassrelease.users import resolve_login
 
 
 def test_resolve_users_from_github_email():
     users = {}
-    assert resolve_user(users, '1000+bla@users.noreply.github.com')
+    assert resolve_login(users, '1000+bla@users.noreply.github.com')
     assert users == {'1000+bla@users.noreply.github.com': 'bla'}
