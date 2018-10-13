208d25fb22708399dee8852942ed872bb5e534c9
WofWca <39462442+WofWca@users.noreply.github.com>
2018-10-13 19:27:32 +0700

    Revert "resolve_login resolves logins by commits"
    It is decided to leave this method alone for now as credits generator is going to resolve users without this it.

--- hassrelease/users.py
+++ hassrelease/users.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 from .const import GH_NO_EMAIL_SUFFIX, USERS_FILE
-from .github import MyGitHub
-# TODO github3.py objects (pr, prs) and bare dicts (repo) are mixed here.
 
 
 def read_csv_to_dict(filename: str):
@@ -28,66 +26,33 @@ def append_dict_to_csv(data: dict, filename: str):
             file.write('{},{}\n'.format(key, value))
 
 
-def resolve_login(login_by_email: dict, email: str, *, pr=None, prs=None,
-                  ask_input=True, context=None, repo: dict=None,
-                  gh: MyGitHub=None):
-    """
-    Tries to resolve the user's email to user's login, and add the pair to
-    'login_by_email'. Returns True if it has been added. Returns False if it is
-    already there or if it failed to resolve the login.
-    :param login_by_email:
-    :param email:
-    :param pr:
-    :param prs:
-    :param ask_input:
-    :param context:
-    :param repo:
-    :param gh: If repo parameter is given, this parameter must be provided too.
-    :return:
-    """
-
-    if email in login_by_email:
+def resolve_login(users, email, *, pr=None, prs=None, ask_input=True, context=None):
+    """Resolves boolean if user added."""
+    if email in users:
         return False
 
-    login = None
+    github = None
 
     if email.endswith(GH_NO_EMAIL_SUFFIX):
         # Strip off suffix
-        userid_and_username = email[:email.index(GH_NO_EMAIL_SUFFIX)]
+        github = email[:email.index(GH_NO_EMAIL_SUFFIX)]
         # Emails are in format <userid>+<username>@suffix. Get username.
-        login = userid_and_username.split('+', 1)[-1]
+        github = github.split('+', 1)[-1]
 
     elif pr is not None:
-        # Find the user by PR.
-        login = prs.get(pr).user.login
-        print('Found {} for {} from PR #{}'.format(login, email, pr))
+        github = prs.get(pr).user.login
+        print('Found {} for {} from PR #{}'.format(github, email, pr))
 
-    elif repo is not None:
-        # Find the user by a commit he made to this repo.
-        # repo['commits_url'] ends with '/commits{/sha}'.  Removing the last 6.
-        commits_url = repo['commits_url'][:-6]
-        commits_response = gh.request_with_retry(
-            url=commits_url,
-            params={
-                'author': email,
-                'per_page': 1
-            })
-        commit = commits_response.json()[0]
-        # Check whether the email is linked to a GitHub profile.
-        if commit['author'] is not None:
-            login = commit['author']['login']
-            # We can also get the user's name right from a commit.
-
-    if login is None:
+    if github is None:
         if ask_input:
             if context is not None:
                 print('Context', context)
-            login = input('GitHub username for {}: '.format(email))
+            github = input('GitHub username for {}: '.format(email))
         else:
             print('Not asking input for {}'.format(email))
             return False
 
-    login_by_email[email] = login
+    users[email] = github
     return True
 
 
