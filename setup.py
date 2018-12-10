7b6e5efb4f0214e0e1eb2310d6014b43a32e15eb
Otto Winter <otto@otto-winter.com>
2018-12-10 10:16:53 +0100

    Upgrade github3 package and remove monkey patch (#14)
    See https://github.com/sigmavirus24/github3.py/blob/master/src/github3/pulls.py#L219

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='hassrelease',
     version='1.0',
     packages=['hassrelease'],
-    install_requires=['github3.py==1.0.0a4', 'click'],
+    install_requires=['github3.py==1.2.0', 'click'],
     entry_points={
         'console_scripts': ['hassrelease = hassrelease.__main__:main']
     },
