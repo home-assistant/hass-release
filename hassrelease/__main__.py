38ffceb9b3ef5c75d10c6ea81e595f2f6327c806
WofWca <wofwca@gmail.com>
2018-12-11 10:32:56 +0800

    Merge latest upstream changes.
    Revert users.csv deletion, rename it to login_by_email.csv

--- hassrelease/__main__.py
+++ hassrelease/__main__.py
@@ -1,9 +1,15 @@
+import click
+
+from .core import HassReleaseError
 from .commands import cli
-from .monkeypatch import patch
+
 
 def main(*args):
-    patch()
-    cli()
+    try:
+        cli()
+    except HassReleaseError as err:
+        click.secho('An error occurred: {}'.format(err), fg='red')
+
 
 if __name__ == '__main__':
     main()
