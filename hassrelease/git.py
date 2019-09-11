13b2d894db66ceb73a50adf11e5069d4432d4c96
Paulus Schoutsen <balloob@gmail.com>
2019-09-11 08:06:58 -0700

    Check dirty before commit

--- hassrelease/git.py
+++ hassrelease/git.py
@@ -9,19 +9,21 @@ def get_hass_version(branch):
     process = subprocess.run(
         "git show {branch}:homeassistant/const.py".format(branch=branch),
         shell=True,
-        cwd='../home-assistant',
+        cwd="../home-assistant",
         stdout=subprocess.PIPE,
-        stderr=subprocess.DEVNULL
+        stderr=subprocess.DEVNULL,
     )
 
     if process.returncode != 0:
-        text = "Failed getting HASS version of branch - Does home-assistant repo exist at " \
-               "../home-assistant? - Does branch {} exist?".format(branch)
+        text = (
+            "Failed getting HASS version of branch - Does home-assistant repo exist at "
+            "../home-assistant? - Does branch {} exist?".format(branch)
+        )
         raise HassReleaseError(text)
 
     locals = {}
     exec(process.stdout, {}, locals)
-    return locals['__version__']
+    return locals["__version__"]
 
 
 def get_log(branch):
@@ -29,20 +31,22 @@ def get_log(branch):
         "git log origin/master...{branch} "
         "--pretty=format:'- %s (%ae)' --reverse".format(branch=branch),
         shell=True,
-        cwd='../home-assistant',
+        cwd="../home-assistant",
         stdout=subprocess.PIPE,
-        stderr=subprocess.DEVNULL
+        stderr=subprocess.DEVNULL,
     )
 
     if process.returncode != 0:
-        text = "Failed getting log - Does home-assistant repo exist at " \
-               "../home-assistant? - Does branch {} exist?".format(branch)
+        text = (
+            "Failed getting log - Does home-assistant repo exist at "
+            "../home-assistant? - Does branch {} exist?".format(branch)
+        )
         raise HassReleaseError(text)
 
-    output = process.stdout.decode('utf-8')
+    output = process.stdout.decode("utf-8")
     last = None
 
-    for line in output.split('\n'):
+    for line in output.split("\n"):
         # Filter out duplicate lines (I don't git very well)
         if line == last:
             continue
@@ -51,26 +55,32 @@ def get_log(branch):
 
 
 def fetch(repo):
-    process = subprocess.run(
-        "git fetch",
-        shell=True,
-        cwd=repo
-    )
+    process = subprocess.run("git fetch", shell=True, cwd=repo)
 
     if process.returncode != 0:
-        text = "Updating Home Assistant repo failed - Does home-assistant repo exist at " \
-               "../home-assistant?"
+        text = (
+            "Updating Home Assistant repo failed - Does home-assistant repo exist at "
+            "../home-assistant?"
+        )
         raise HassReleaseError(text)
 
 
-def cherry_pick(sha, cwd='../home-assistant'):
-    process = subprocess.run(
-        "git cherry-pick {}".format(sha),
-        shell=True,
-        cwd=cwd
-    )
+def cherry_pick(sha, cwd="../home-assistant"):
+    process = subprocess.run("git cherry-pick {}".format(sha), shell=True, cwd=cwd)
 
     if process.returncode != 0:
-        text = "Cherry picking {} failed - Does home-assistant repo exist at " \
-               "../home-assistant?".format(sha)
+        text = (
+            "Cherry picking {} failed - Does home-assistant repo exist at "
+            "../home-assistant?".format(sha)
+        )
         raise HassReleaseError(text)
+
+
+def is_dirty(repo):
+    """Test if repo is dirty."""
+    return (
+        subprocess.run(
+            "git diff --stat", capture_output=True, shell=True, cwd=repo
+        ).stdout
+        != b""
+    )
