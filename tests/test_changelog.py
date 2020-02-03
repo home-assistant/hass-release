8599bae9a9b5c248a34c910c2b3e59fc92613b98
Paulus Schoutsen <balloob@gmail.com>
2020-02-02 16:32:34 -0800

    Fix tests

--- tests/test_changelog.py
+++ tests/test_changelog.py
@@ -2,26 +2,29 @@ from hassrelease.changelog import automation_link, _process_doc_label
 
 
 def test_automation_link():
-    assert automation_link('automation.mqtt') == \
-        ('[automation.mqtt docs]: '
-         'https://home-assistant.io/docs/automation/trigger/#mqtt-trigger')
+    assert automation_link("automation.mqtt", False) == (
+        "[automation.mqtt docs]: "
+        "https://www.home-assistant.io/docs/automation/trigger/#mqtt-trigger"
+    )
 
-    assert automation_link('automation.homeassistant') == \
-        ('[automation.homeassistant docs]: '
-         'https://home-assistant.io/docs/automation/trigger/'
-         '#home-assistant-trigger')
+    assert automation_link("automation.homeassistant", False) == (
+        "[automation.homeassistant docs]: "
+        "https://www.home-assistant.io/docs/automation/trigger/"
+        "#home-assistant-trigger"
+    )
 
-    assert automation_link('automation.numeric_state') == \
-        ('[automation.numeric_state docs]: '
-         'https://home-assistant.io/docs/automation/trigger/'
-         '#numeric-state-trigger')
+    assert automation_link("automation.numeric_state", False) == (
+        "[automation.numeric_state docs]: "
+        "https://www.home-assistant.io/docs/automation/trigger/"
+        "#numeric-state-trigger"
+    )
 
 
 def test_process_doc_label():
     links = set()
     parts = []
 
-    _process_doc_label('platform: light.hue', parts, links)
+    _process_doc_label("integration: hue", parts, links, False)
 
-    assert parts[-1] == '([light.hue docs])'
-    assert next(iter(links)).startswith('[light.hue docs]')
+    assert parts[-1] == "([hue docs])"
+    assert next(iter(links)).startswith("[hue docs]")
