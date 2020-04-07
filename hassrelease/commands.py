89774027b28cf56d7e481d9519eadae634ffa25c
Paulus Schoutsen <balloob@gmail.com>
2020-04-07 10:25:00 -0700

    Fix another None exception

--- hassrelease/commands.py
+++ hassrelease/commands.py
@@ -128,7 +128,7 @@ def pick(repo, milestone):
     print("Just Picked")
     print()
     for pull, _ in to_pick:
-        if pull == failed_pick:
+        if failed_pick == pull:
             break
         print(f"- {pull.title} (@{pull.user.login} - #{pull.number})")
 
