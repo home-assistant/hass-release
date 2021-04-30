e408af7f4f394dffc9f092743cb48256754c8b3e
Bram Kragten <mail@bramkragten.nl>
2021-04-30 18:37:07 +0200

    Update `bump_frontend` command to my workflow

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='hassrelease',
-    version='1.0',
-    packages=['hassrelease'],
-    install_requires=['github3.py==1.2.0', 'click', 'pystache', 'requests'],
-    entry_points={
-        'console_scripts': ['hassrelease = hassrelease.__main__:main']
-    },
+    name="hassrelease",
+    version="1.0",
+    packages=["hassrelease"],
+    install_requires=["github3.py==1.2.0", "click", "pystache", "requests"],
+    entry_points={"console_scripts": ["hassrelease = hassrelease.__main__:main"]},
 )
