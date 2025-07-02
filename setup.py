7284ce6decf914ecb31b31931347780357f2bba5
Paulus Schoutsen <balloob@gmail.com>
2025-07-02 07:56:32 +0200

    Add packaging for Python 3.12 compat

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="hassrelease",
     version="1.0",
     packages=["hassrelease"],
-    install_requires=["github3.py==3.2.0", "click", "pystache", "requests", "toml"],
+    install_requires=["github3.py==3.2.0", "click", "pystache", "requests", "toml", "packaging"],
     entry_points={"console_scripts": ["hassrelease = hassrelease.__main__:main"]},
 )
